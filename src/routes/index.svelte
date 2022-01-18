2ae70c071f832a2244e82fe0efbb4701724c4d1f
kevinnls <57634663+kevinnls@users.noreply.github.com>
2022-01-18 17:58:10 +0530

    header conf

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,13 @@
 	import History from '../components/History.svelte';
 </script>
 
+<svelte:head>
+	<title>WhatsApp Dialer</title>
+	<meta
+		name="description"
+		content="Quickly start a WhatsApp conversation without needing to save a contact" />
+</svelte:head>
+
 <header>
 	<h1>WhatsApp Dialer</h1>
 </header>
@@ -14,8 +21,6 @@
 
 <footer>
 	<small>
-		&copy; 2022 Kevin Samuel <a href="mailto:developer+whatsappdialer@kevinnlsamuel.com">
-			&lt;developer@kevinnlsamuel.com&gt;
-		</a>
+		&copy; 2022 Kevin Samuel <a href="mailto:developer+whatsappdialer@kevinnlsamuel.com"> &lt;developer@kevinnlsamuel.com&gt; </a>
 	</small>
 </footer>
